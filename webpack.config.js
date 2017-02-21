01c58679667ee49894129e38a53d7c3161b020b4
eavichay <eavichay@gmail.com>
2017-02-21 23:25:42 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+// Include Section
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Seed Project',
+            filename: 'index.html',
+            template: 'index.tmpl.html',
+            inject: 'body',
+            hash: true
+        })
+    ]
+};
