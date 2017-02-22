85edb9d1e5ba39d10ea2a5ea239e76acf350eae0
eavichay <eavichay@gmail.com>
2017-02-22 21:03:38 +0200

    API

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 // Include Section
 
 module.exports = {
+    devtool: 'source-map',
     entry: './app/index.js',
     output: {
         filename: 'bundle.js',
